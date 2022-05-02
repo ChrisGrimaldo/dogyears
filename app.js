2cd8d64fccc0067ab3eaf632eae787693d6ec9dc
ChrisGrimaldo <62303962+ChrisGrimaldo@users.noreply.github.com>
2022-05-02 10:00:16 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+const myAge = 27; //declared myAge and stored 27
+let earlyYears = 2; //declared earlyYears and stored 2
+earlyYears = earlyYears * 10.5;
+let laterYears = myAge - 2; //declared laterYears
+laterYears * 4; // Calculate number of dog years accounted for by your later years
+console.log(earlyYears);
+console.log(laterYears);
+var myAgeInDogYears = earlyYears + laterYears;
+var myName = "Chris".toLowerCase(myName);
+console.log(
+  `My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`
+);
